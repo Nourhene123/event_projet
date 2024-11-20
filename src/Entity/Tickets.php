e8352ed92efeaab34a586b5d629de428efc1f5a5
nourhene ferchichi <ferchichinour91@gmail.com>
2024-11-20 17:44:42 +0000

    Revert "création des contrôleurs et des routes, ainsi que du contrôleur utilisateur..."
    This reverts commit 88544a3d51923cf27cf3b17f31b0138e13c8559e

---
+++ src/Entity/Tickets.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\TicketsRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: TicketsRepository::class)]
+class Tickets
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column]
+    private ?int $price = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $type = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getPrice(): ?int
+    {
+        return $this->price;
+    }
+
+    public function setPrice(int $price): static
+    {
+        $this->price = $price;
+
+        return $this;
+    }
+
+    public function getType(): ?string
+    {
+        return $this->type;
+    }
+
+    public function setType(string $type): static
+    {
+        $this->type = $type;
+
+        return $this;
+    }
+}
