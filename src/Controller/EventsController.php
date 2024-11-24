c9414a81ad5ae398a5e29c96de39dd48c1d86f18
Jesser mdimegh <jessermdimagh@gmail.com>
2024-11-24 10:04:29 +0100

    Reserver Event

--- src/Controller/EventsController.php
+++ src/Controller/EventsController.php
@@ -2,22 +2,33 @@
 
 namespace App\Controller;
 
+use App\Entity\Reservation;
 use App\Repository\EventsRepository;
+use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class EventsController extends AbstractController
-{
+{ private $em;
+    public function __construct(EntityManagerInterface $em) { $this->em = $em; }
     #[Route('/events', name: 'app_event')]
-    public function listEvents(EventsRepository $er): Response
+    public function listEvents(EventsRepository $er,UserRepository $ur): Response
     {
+        $user=$ur->find(1);
+        $reservations = $this->em->getRepository(Reservation::class)->findBy(['User' => $user]);
+        $registeredEvents = array_map(function($reservation) {
+            return $reservation->getEvent()->getId(); },
+            $reservations);
         $events = $er->findAll();
         return $this->render('events/listEvents.html.twig', [
             'events' => $events,
             'field' => "",
             'value' => "",
+            'registeredEvents' => $registeredEvents,
         ]);
     }
 
